00c98488d5295c6cac94e56f706e4555a68c8032
Beha <shacknetisp@mail.com>
2017-02-09 22:27:27 -0500

    Independent mapgen

--- mapgen.lua
+++ mapgen.lua
@@ -1,13 +1,11 @@
-function magic.register_mapgen()
-    -- Vitality crystals form in warm forests.
-    minetest.register_decoration({
-        deco_type = "simple",
-        place_on = {"default:dirt_with_grass"},
-        sidelen = 80,
-        fill_ratio = 0.00025,
-        biomes = {"rainforest", "deciduous_forest", "coniferous_forest"},
-        y_min = 1,
-        y_max = 31000,
-        decoration = "magic:crystal_vitality",
-    })
-end
+-- Vitality crystals form in warm forests.
+minetest.register_decoration({
+    deco_type = "simple",
+    place_on = {"default:dirt_with_grass"},
+    sidelen = 80,
+    fill_ratio = 0.00025,
+    biomes = {"rainforest", "deciduous_forest", "coniferous_forest"},
+    y_min = 1,
+    y_max = 31000,
+    decoration = "magic:crystal_vitality",
+})
